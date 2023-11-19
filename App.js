747b2d57b3cb7d1130b5fe6113dfd25e1582b440
padoniachristine <143053303+padoniachristine@users.noreply.github.com>
2023-11-18 14:40:18 -1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,95 @@
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import { users } from "./User";
+import { TouchableOpacity } from "react-native-web";
+
+
+export default function App() {
+  const showFullname = (users) => {
+    alert(users)
+  };
+
+  return (
+    <View style = {styles.container}>
+      <ScrollView>
+      <View style = {styles.content}>
+        <FlatList
+          data = {users}
+          renderItem = {({item}) => (
+            <View style = {styles.row}>
+              <View style = {styles.initial}>
+                <Text style = {styles.initialText}>
+                  {item.name.lastname.charAt(0).toUpperCase()}
+                  {item.name.firstname.charAt(0).toUpperCase()}
+                </Text>
+              </View>
+              <View style = {styles.details}>
+                <Text style = {styles.name}>
+                  {item.name.lastname}, {item.name.firstname}
+                  {item.name.middlename ? ` ${item.name.middlename}.` : ""}
+                </Text>
+                <Text style = {styles.address}>{item.address}</Text>
+              </View>
+              <TouchableOpacity onPress = {() => showFullname(`${'Name: '}${item.name.firstname}${item.name.lastname}${'\n'}${'Nickname: '}${item.nickname}${'\n'}${'Address: '}${item.address}${'\n'}${'Course and Year: '}${item.courseAndYear}`)}>
+              <Icon name = "dots-three-horizontal" size={20} color="black" />
+              </TouchableOpacity>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      </ScrollView>
+      <StatusBar style = "auto" />
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    paddingBottom: 10,
+  },
+  content: {
+    justifyContent: "flex-end",
+  },
+  name: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  address: {
+    color: "black",
+    fontSize: 14,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  },
+  initial: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#56b39d",
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  initialText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  details: {
+    flex: 1,
+    marginLeft: 10,
+  },
+});
---
+++ User.js
@@ -0,0 +1,101 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "CHRISTINE",
+        middlename: "N",
+        lastname: "PADONIA",
+      },
+      nickname: 'Tine',
+      courseAndYear: 'IT3',
+      address: "De La Paz, Cortes",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "ANALIE",
+        middlename: "V",
+        lastname: "BARBANIDA",
+      },
+      nickname: 'Antet',
+      courseAndYear: 'IT3',
+      address: "Cabawan, Maribojoc",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "LAIZA",
+        middlename: "",
+        lastname: "HANGAD",
+      },
+      nickname: 'Lai',
+      courseAndYear: 'IT3',
+      address: "Del Carmen Weste, Balilihan",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "JUNAH",
+        middlename: "J",
+        lastname: "MASLOG",
+      },
+      nickname: 'Onang',
+      courseAndYear: 'CRIM4',
+      address: "Manga District, Tagbilaran City",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "QUENNIE",
+        middlename: "",
+        lastname: "EULLONE",
+      },
+      nickname: 'Kwen',
+      courseAndYear: 'FM4',
+      address: "Agahay, Maribojoc",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "JEA MARIE",
+        middlename: "",
+        lastname: "OCHEA",
+      },
+      nickname: 'Eyang',
+      courseAndYear: 'AMT1',
+      address: "Mansasa, Tagbilaran City",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "ERECA",
+        middlename: "",
+        lastname: "GREPO",
+      },
+      nickname: 'Ecang',
+      courseAndYear: 'AMT1',
+      address: "Mayacabac, Dauis",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "CARA",
+        middlename: "",
+        lastname: "ARANZADO",
+      },
+      nickname: 'Cay',
+      courseAndYear: 'HM3',
+      address: "Tinago, Dauis",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "JAQUELINE",
+        middlename: "",
+        lastname: "VILLOREJO",
+      },
+      nickname: 'Jaq',
+      courseAndYear: 'IT3',
+      address: "Cabawan, Maribojoc",
+    },
+  ];
